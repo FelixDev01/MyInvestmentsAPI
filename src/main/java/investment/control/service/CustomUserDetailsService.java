e864e31a0ca24d0a517b9be9f61f6a6ad9b9af8f
Felix Eduardo Villanueva Condori <felixeduardobx@gmail.com>
2025-09-18 09:05:20 -0300

    Security Reconfigured

--- src/main/java/investment/control/service/CustomUserDetailsService.java
+++ src/main/java/investment/control/service/CustomUserDetailsService.java
@@ -1,34 +1,4 @@
 package investment.control.service;
 
-import investment.control.repository.UserRepository;
-import investment.control.model.User;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.stream.Collectors;
-
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-
-    private final UserRepository repository;
-
-    public CustomUserDetailsService(UserRepository repository) {
-        this.repository = repository;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
-        User user = repository.findByEmail(email)
-            .orElseThrow(()-> new UsernameNotFoundException("User not Found"));
-
-        var authorities = user.getRoles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getEmail(),
-                user.getPassword(),
-                authorities);
-    }
+public class CustomUserDetailsService {
 }
